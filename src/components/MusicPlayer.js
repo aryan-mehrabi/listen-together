50f21cce0baa53b4ef5cf782d184b288a9f2f101
aryan <aryan.me77@yahoo.com>
2022-06-11 20:02:13 +0430

    sync playing add

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -1,12 +1,53 @@
 import React, { useEffect, useRef, useState } from "react";
+import useChannel from "../context/ChannelContext";
 
 const MusicPlayer = ({ widget }) => {
   const ref = useRef();
-  const [track, setTrack] = useState("303102630");
+  const { channels, selectedChannel, playTrack, pauseTrack, seekTrack } =
+    useChannel();
+  const { track, position, isPlaying } = channels[selectedChannel];
 
   useEffect(() => {
     widget.current = window.SC.Widget(ref.current);
+    widget.current.bind(window.SC.Widget.Events.READY, () => {
+      widget.current.bind(window.SC.Widget.Events.PLAY, () => {
+        // do this on click play
+        widget.current.getPosition(position => {
+          console.log("play on event handlere");
+          playTrack(position);
+        });
+      });
+      widget.current.bind(window.SC.Widget.Events.PAUSE, () => {
+        // do this on click pause
+        console.log("pause on event handlere");
+        pauseTrack();
+      });
+      widget.current.bind(window.SC.Widget.Events.SEEK, () => {
+        // do this on click seek
+        widget.current.getPosition(position => {
+          seekTrack(position)
+        });
+      });
+    });
   }, []);
+
+  useEffect(() => {
+    widget.current.bind(window.SC.Widget.Events.READY, () => {
+      widget.current.isPaused(isPaused => {
+        if (isPlaying && isPaused) {
+          widget.current.play();
+        } else if (!isPlaying && !isPaused) {
+          widget.current.pause();
+        }
+      });
+    });
+  }, [isPlaying]);
+
+  useEffect(() => {
+    widget.current.bind(window.SC.Widget.Events.READY, () => {
+      widget.current.seekTo(position);
+    });
+  }, [position]);
 
   return (
     <iframe
@@ -17,7 +58,7 @@ const MusicPlayer = ({ widget }) => {
       scrolling="no"
       frameBorder="no"
       allow="autoplay"
-      src={`https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${track}&color=%231a1d1e&auto_play=false&hide_related=false&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true&download=false&sharing=false&single_active=false`}
+      src={`https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${track}&color=%231a1d1e&auto_play=false&hide_related=false&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&download=false&sharing=false&single_active=false`}
     ></iframe>
   );
 };
