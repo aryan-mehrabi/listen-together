7e4999dde5a24c969147a293ede0c0089f7cce5f
aryan <aryan.me77@yahoo.com>
2022-06-14 14:42:32 +0430

    time add to messages

--- src/components/ChannelMessage.js
+++ src/components/ChannelMessage.js
@@ -3,22 +3,38 @@ import useAuth from "../context/AuthContext";
 
 const ChannelMessage = ({ message }) => {
   const { userId } = useAuth();
+  const { createdAt, from, content, avatar, name } = message;
 
-  if (userId === message.from) {
+  const getMessageTime = () => {
+    const time = createdAt?.toDate();
+    if (!time) return <i class="fa-solid fa-clock text-[11px]"></i>;
+
+    const hour = time.getHours().toString();
+    const minute = time.getMinutes().toString();
+    return `${hour}:${minute.length === 1 ? "0" : ""}${minute}`;
+  };
+
+  if (userId === from) {
     return (
       <div className="ml-auto my-1 max-w-[80%]">
-        <p className="bg-secondary text-primary rounded-sm p-2 ml-2">
-          {message.content}
-        </p>
+        <div className="bg-secondary rounded-sm p-2 ml-2">
+          <p className="text-primary">{content}</p>
+          <p className="text-primary opacity-75 mt-1 mb-[-4px] text-sm text-right">
+            {getMessageTime()}
+          </p>
+        </div>
       </div>
     );
   } else {
     return (
       <div className="flex items-start my-1 max-w-[80%]">
-        <img className="w-10 mt-2" src={message.avatar} alt="avatar" />
+        <img className="w-10 mt-2" src={avatar} alt="avatar" />
         <div className="bg-neutral-700 rounded-sm p-2 ml-2">
-          <h6 className="text-lg font-semibold text-cta">{message.name}</h6>
-          <p>{message.content}</p>
+          <h6 className="text-lg font-semibold text-cta">{name}</h6>
+          <p>{content}</p>
+          <p className="text-sm opacity-75 mt-1 mb-[-4px] text-right">
+            {getMessageTime()}
+          </p>
         </div>
       </div>
     );
