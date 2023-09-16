edbb402f185a3666926b412fbea240ff1ed64ae4
aryan <aryan.me77@yahoo.com>
2023-09-17 01:52:06 +0330

    feat: add kick out

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo } from "react";
 import Channel from "feature/Channel";
 import Sidebar from "feature/Sidebar";
 import useChannel from "context/ChannelContext";
 import useMediaQuery from "hooks/useMediaQuery";
 import { RightSidebarProvider } from "context/RightSidebarContext";
 import useMember from "context/MemberContext";
+import useAuth from "context/AuthContext";
 
 const Chat = () => {
   const { fetchUsersMember, subscribeUsersMember } = useMember();
-  const { selectedChannel } = useChannel();
+  const { selectedChannel, setSelectedChannel } = useChannel();
+  const { members } = useMember();
+  const { userId } = useAuth();
   const isMobile = useMediaQuery("screen and (max-width: 640px");
 
   useEffect(() => {
     fetchUsersMember();
-    const unsubscribe = subscribeUsersMember()
+    const unsubscribe = subscribeUsersMember();
     return () => unsubscribe();
   }, []);
+
+  useEffect(() => {
+    const memberShip = Object.values(members).find(
+      member =>
+        member.user_id === userId && member.channel_id === selectedChannel
+    );
+    if (!memberShip) {
+      setSelectedChannel("");
+    }
+  }, [selectedChannel, members, userId]);
 
   const mobile = selectedChannel ? <Channel /> : <Sidebar />;
 
