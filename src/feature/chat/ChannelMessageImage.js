d5a5c299747f8c5c7f4320628e5072ea2367ed62
aryan <aryan.me77@yahoo.com>
2024-03-18 12:54:20 +0330

    feat: add image placeholder

--- src/feature/chat/ChannelMessageImage.js
+++ src/feature/chat/ChannelMessageImage.js
@@ -3,27 +3,33 @@ import Spinner from "components/Spinner";
 import useFetchImage from "hooks/useFetchImage";
 import useModal from "context/ModalContext";
 import ImageBlob from "components/ImageBlob";
+import ImagePlaceholder from "assets/placeholder-image.png";
 
 const ChannelMessageImage = ({ image }) => {
   const { data, status, refetch } = useFetchImage(image.url);
   const { setModal } = useModal();
 
-  if (status === "idle")
+  const idleButton = (
+    <div className="flex items-center justify-center w-14 h-14 text-primary border-2 border-primary p-3 rounded-full">
+      <i className="fa-solid fa-arrow-down text-2xl"></i>
+    </div>
+  );
+  const loadingButton = <Spinner className="w-14 h-14" />;
+
+  if (status === "idle" || status === "loading")
     return (
       <div
         onClick={() => refetch()}
-        className="flex justify-center items-center h-full"
+        className="h-full bg-slate-300"
+        style={{
+          backgroundImage: `url(${ImagePlaceholder})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
       >
-        <div className="flex items-center justify-center w-14 h-14 border-2 border-primary p-3 rounded-full">
-          <i className="fa-solid fa-arrow-down text-2xl"></i>
+        <div className="flex justify-center items-center backdrop-blur-md w-full h-full">
+          {status === "idle" ? idleButton : loadingButton}
         </div>
-      </div>
-    );
-
-  if (status === "loading")
-    return (
-      <div className="flex justify-center items-center h-full">
-        <Spinner className="w-14 h-14" />
       </div>
     );
 
--- src/feature/chat/ChannelMessageReplyImage.js
+++ src/feature/chat/ChannelMessageReplyImage.js
@@ -1,8 +1,12 @@
 import ImageBlob from "components/ImageBlob";
 import useFetchImage from "hooks/useFetchImage";
+import ImagePlaceholder from "assets/placeholder-image.png";
 
 export default function ChannelMessageReplyImage({ image }) {
   const { data } = useFetchImage(image.url);
-  if (!data) return;
+  if (!data)
+    return (
+      <img src={ImagePlaceholder} className="w-full h-full object-cover" />
+    );
   return <ImageBlob blob={data} />;
 }
