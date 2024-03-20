3f2221908bd02dd826216990fdc7a11bed226a61
aryan <aryan.me77@yahoo.com>
2024-03-20 14:41:13 +0330

    fix: overlaping input text and upload image icon

--- src/feature/chat/ChannelMessageInput.js
+++ src/feature/chat/ChannelMessageInput.js
@@ -52,7 +52,7 @@ const ChannelMessageInput = () => {
         onSubmit={onSubmitForm}
         className="flex p-3 border-t border-neutral-700"
       >
-        <div className="relative flex-grow h-10">
+        <div className="flex-grow h-10">
           <input
             onChange={onChangeFileInput}
             accept="image/jpeg, image/jpg, image/png, image/gif"
@@ -61,22 +61,24 @@ const ChannelMessageInput = () => {
             className="hidden"
             ref={fileInputRef}
           />
-          <textarea
-            onPaste={onPasteMessageInput}
-            onKeyDown={onKeyPressEnter}
-            onChange={(e) => setMessage(e.target.value.trimStart())}
-            value={message}
-            placeholder="Type a message"
-            className="w-full bg-neutral-700 h-full rounded outline-none p-2 resize-none"
-            type="text"
-          />
-          <button
-            type="button"
-            className="absolute right-4 top-1/2 -translate-y-1/2 text-xl"
-            onClick={() => fileInputRef.current.click()}
-          >
-            <i className="fa fa-file-image" aria-hidden="true"></i>
-          </button>
+          <div className="flex items-center gap-2 w-full h-full bg-neutral-700 rounded p-2 pr-3">
+            <textarea
+              onPaste={onPasteMessageInput}
+              onKeyDown={onKeyPressEnter}
+              onChange={(e) => setMessage(e.target.value.trimStart())}
+              value={message}
+              placeholder="Type a message"
+              className="bg-transparent w-full h-full outline-none resize-none"
+              type="text"
+            />
+            <button
+              type="button"
+              className="text-xl"
+              onClick={() => fileInputRef.current.click()}
+            >
+              <i className="fa fa-file-image" aria-hidden="true"></i>
+            </button>
+          </div>
         </div>
         <Button
           type="cta"
