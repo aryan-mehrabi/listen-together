070a12993f592cf57a56e6d2d5a71c24c6504dea
aryan <aryan.me77@yahoo.com>
2024-04-04 12:45:03 +0330

    chore: remove dead code

--- src/context/ChannelContext/channelReducer.js
+++ src/context/ChannelContext/channelReducer.js
@@ -1,33 +1,20 @@
 const channelReducer = (state, { type, payload }) => {
   switch (type) {
-    case "FETCH_MESSAGES":
-      const messages = {};
-      payload.data.forEach(message => {
-        messages[message.data().id] = { ...message.data() };
-      });
-      return {
-        ...state,
-        [payload.channelId]: {
-          ...state[payload.channelId],
-          messages: { ...messages },
-        },
-      };
-
-    case "DELETE_CHANNEL":
-      const { [payload]: a, ...newChannels } = state;
-      return {
-        ...newChannels,
-      };
     case "FETCH_CHANNEL":
       return { ...state, [payload.id]: payload };
     case "FETCH_CHANNELS":
       const channels = {};
-      payload.forEach(channel => {
+      payload.forEach((channel) => {
         channels[channel.id] = channel;
       });
       return { ...state, ...channels };
     case "UPDATE_CHANNEL":
       return { ...state, [payload.id]: payload };
+    case "DELETE_CHANNEL":
+      const { [payload]: a, ...newChannels } = state;
+      return {
+        ...newChannels,
+      };
     default:
       return state;
   }
