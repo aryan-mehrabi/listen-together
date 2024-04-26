f5982947433fd63f886792aa3fba13fbfbcc623b
aryan <aryan.me77@yahoo.com>
2024-04-26 19:03:20 +0330

    feat: fix modal ui

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import useModal from "context/ModalContext";
+import Button from "./Button";
 
 const Modal = ({ children }) => {
   const { setModal } = useModal();
@@ -8,11 +9,14 @@ const Modal = ({ children }) => {
   return ReactDOM.createPortal(
     <div
       onClick={() => setModal(null)}
-      className="absolute inset-0 overflow-auto bg-neutral-700 bg-opacity-50 text-secondary flex items-center justify-center"
+      className="absolute inset-0 overflow-auto bg-neutral-800 bg-opacity-90 text-secondary flex items-center justify-center"
     >
-      <div className="fixed top-3 right-9 cursor-pointer">
-        <i className="fa-solid fa-xmark text-4xl [text-shadow:_0_2px_0_rgb(0_0_0_/_80%)]"></i>
-      </div>
+      <Button
+        type="primary"
+        className="fixed top-3 right-3 flex items-center justify-center w-9 h-9 bg-black bg-opacity-60"
+      >
+        <i className="fa-solid fa-xmark text-xl"></i>
+      </Button>
       <div
         onClick={(e) => e.stopPropagation()}
         className="max-w-full max-h-full"
