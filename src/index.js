d9bf7724be0d7ed9364f5df2e2603f13b1ede203
aryan <aryan.me77@yahoo.com>
2024-05-03 16:55:38 +0330

    feat: add sentry

--- src/index.js
+++ src/index.js
@@ -2,7 +2,25 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App";
 import "./index.css";
+import * as Sentry from "@sentry/react";
 
+Sentry.init({
+  dsn: "https://1d0bc1f3523a42de427e6944debe14ed@o4507157705261056.ingest.us.sentry.io/4507157736718336",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: [
+    "localhost",
+    "https://listen-together-aryan.netlify.app/",
+  ],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 const container = document.querySelector("#root");
 const root = createRoot(container);
 root.render(
