d094853c07c937fc3d782ef8f3f7a0bd4d94ef6b
aryan <aryan.me77@yahoo.com>
2024-09-18 22:15:52 +0330

    feat: prevent img being draggable

--- src/components/ImageBlob.js
+++ src/components/ImageBlob.js
@@ -3,6 +3,7 @@ import { overrideTailwindClasses } from "tailwind-override";
 export default function ImageBlob({ blob, className = "", alt }) {
   return (
     <img
+      draggable="false"
       className={overrideTailwindClasses(
         `w-full h-full object-cover ${className}`
       )}
