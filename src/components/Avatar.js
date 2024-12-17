c6885feca0d39e7a741252f3333161f0d69727b7
aryan <aryan.me77@yahoo.com>
2024-12-18 00:16:33 +0330

    fix: user avatar image width

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -2,12 +2,12 @@ import { overrideTailwindClasses } from "tailwind-override";
 
 export const Avatar = ({ src, showListening, className = "" }) => {
   return (
-    <div className="mt-2 relative">
-      <img
-        className={overrideTailwindClasses(`w-10 ${className}`)}
-        src={src}
-        alt="avatar"
-      />
+    <div
+      className={overrideTailwindClasses(
+        `shrink-0 mt-2 relative w-10 ${className}`
+      )}
+    >
+      <img className="w-full" src={src} alt="avatar" />
       {showListening && (
         <div className="w-6 h-6 animate-motioning grid items-center justify-center absolute translate-y-1/2 bottom-0 right-0 bg-secondary bg-opacity-90 rounded-full">
           <i className="fa-solid fa-music text-xs fa-fade text-cta"></i>
