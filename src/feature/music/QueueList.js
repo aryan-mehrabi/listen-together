2893080460f685f7a70f9c730fb5d02c459b59ea
aryan <aryan.me77@yahoo.com>
2025-01-03 15:06:40 +0330

    feat: implement drag-and-drop functionality for track reordering in QueueList

--- src/feature/music/QueueList.js
+++ src/feature/music/QueueList.js
@@ -2,56 +2,73 @@ import supabase from "auth/supabase";
 import useChannel from "context/ChannelContext";
 import useTrack from "context/TrackContext";
 import { BiTrash } from "react-icons/bi";
+import { closestCenter, DndContext, DragOverlay } from "@dnd-kit/core";
+import {
+  SortableContext,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import QueueListItem from "./QueueListItem";
+import { createPortal } from "react-dom";
+import { useState } from "react";
 
 export default function QueueList() {
-  const { tracks, fetchTracks } = useTrack();
+  const { tracks } = useTrack();
   const { selectedChannel, channels } = useChannel();
+  const [activeId, setActiveId] = useState("");
   const channel = channels[selectedChannel];
   const channelTracks = Object.values(tracks[selectedChannel] || {});
 
-  const handleRemoveTrackFromQueue = async (track) => {
-    await supabase.rpc("delete_track_from_queue", {
-      _id: track.id,
-      _playlist_id: track.playlist_id,
-    });
+  const handleDragStart = (event) => {
+    console.log(event);
+    setActiveId(event.active.id);
+  };
 
-    fetchTracks(selectedChannel, channel.playlists.id);
+  const handleDragEnd = (event) => {
+    setActiveId("");
   };
 
   return (
     <>
       <h2 className="text-2xl font-semibold p-5">Queue</h2>
-      <ul className="overflow-y-auto">
-        {channelTracks
-          .sort((a, b) => a.position - b.position)
-          .filter(
-            (track) =>
-              track.position >=
-              tracks[selectedChannel][channel.track_id].position
-          )
-          .map((track) => (
-            <li
-              key={track.id}
-              className={`${
-                track.id === channel.track_id ? "bg-neutral-700" : ""
-              } flex items-center gap-2 p-4 border-b-[1px] border-neutral-700 first-of-type:border-t-[1px]`}
-            >
-              <img
-                src={track.metadata?.thumbnail}
-                className="w-12"
-                alt="track thumbnail"
-              />
-              <p>{track.metadata?.title || "untitled"}</p>
-              <button
-                type="button"
-                className="ml-auto"
-                onClick={() => handleRemoveTrackFromQueue(track)}
-              >
-                <BiTrash />
-              </button>
-            </li>
-          ))}
-      </ul>
+      <DndContext
+        collisionDetection={closestCenter}
+        onDragStart={handleDragStart}
+        onDragEnd={handleDragEnd}
+      >
+        <SortableContext
+          strategy={verticalListSortingStrategy}
+          items={channelTracks
+            .sort((a, b) => a.position - b.position)
+            .filter(
+              (track) =>
+                track.position >=
+                tracks[selectedChannel][channel.track_id].position
+            )
+            .map((track) => track.id)}
+        >
+          <ul className="overflow-y-auto">
+            {channelTracks
+              .sort((a, b) => a.position - b.position)
+              .filter(
+                (track) =>
+                  track.position >=
+                  tracks[selectedChannel][channel.track_id].position
+              )
+              .map((track) => (
+                <QueueListItem key={track.id} track={track} channel={channel} />
+              ))}
+          </ul>
+        </SortableContext>
+        <DragOverlay dropAnimation={null}>
+          {activeId && (
+            <QueueListItem
+              track={tracks[selectedChannel][activeId]}
+              channel={channel}
+              isOverlay
+            />
+          )}
+        </DragOverlay>
+      </DndContext>
     </>
   );
 }
---
+++ src/feature/music/QueueListItem.js
@@ -0,0 +1,56 @@
+import { useSortable } from "@dnd-kit/sortable";
+import supabase from "auth/supabase";
+import { BiTrash } from "react-icons/bi";
+import { overrideTailwindClasses } from "tailwind-override";
+
+export default function QueueListItem({ track, channel, isOverlay }) {
+  const sortable = useSortable({
+    id: track.id,
+  });
+
+  const isBottom = sortable.overIndex > sortable.activeIndex;
+
+  const handleRemoveTrackFromQueue = async (track) => {
+    await supabase.rpc("delete_track_from_queue", {
+      _id: track.id,
+      _playlist_id: track.playlist_id,
+    });
+  };
+
+  const indicatorStyles = `after:w-full after:h-[2px] after:absolute after:right-0 after:bg-red-500`;
+
+  return (
+    <li
+      {...sortable.listeners}
+      {...sortable.attributes}
+      ref={sortable.setNodeRef}
+      className={overrideTailwindClasses(`${isOverlay ? "opacity-30" : ""} ${
+        track.id === channel.track_id ? "bg-neutral-700" : ""
+      } flex items-center gap-2 p-4 border-b-[1px] border-neutral-700 first-of-type:border-t-[1px] relative
+      ${
+        sortable.isOver
+          ? isBottom
+            ? ` ${indicatorStyles} after:bottom-0`
+            : ` ${indicatorStyles} after:top-0`
+          : ""
+      }
+      `)}
+    >
+      <img
+        src={track.metadata?.thumbnail}
+        className="w-12"
+        alt="track thumbnail"
+      />
+      <p>{track.metadata?.title || "untitled"}</p>
+      {track.id !== channel.track_id && (
+        <button
+          type="button"
+          className="ml-auto"
+          onClick={() => handleRemoveTrackFromQueue(track)}
+        >
+          <BiTrash />
+        </button>
+      )}
+    </li>
+  );
+}
